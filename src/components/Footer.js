a020e811bcb2d0b33f1f5c55fbc5642f0f6c5f66
xxolkim <dev.xxolkim@gmail.com>
2023-10-20 17:20:31 +0900

    feat : 프로젝트 파일 정리 및 최적화

---
+++ src/components/Footer.js
@@ -0,0 +1,3 @@
+export default function Footer() {
+  return <footer class="footer">하단</footer>;
+}
---
+++ src/components/Header.js
@@ -0,0 +1,4 @@
+export default function Header() {
+  // javascript 코드 자리
+  return <header class="header">상단</header>;
+}
